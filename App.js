e29aab3084d34d6ee941b41cd0dc14a1f49bce10
Amar Kayed <amar.kayed18@gmail.com>
2022-02-26 21:17:04 +0200

    Added <Image /> Component

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => console.log("Text Pressed")
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text numberOfLines={1} onPress={()=>{console.log("text has been clicked")}}>a relatively relative really rally longly long texty text that is long and a lot of text written in the text tag.</Text>
+      <Text numberOfLines={1} onPress={handlePress}>a relatively relative really rally longly long texty text that is long and a lot of text written in the text tag.</Text>
+      {/* <Image source = {require('./assets/icon.png')} /> */}
+      <Image 
+      blurRadius={10}
+      fadeDuration = {1000}
+      source = {{
+        width: 200,
+        height: 300,
+        uri: 'https://picsum.photos/200/300'}} />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +29,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 });
