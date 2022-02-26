aca8c03f178e8c53b86fd512aa070c5fb43b05e3
Amar Kayed <amar.kayed18@gmail.com>
2022-02-26 21:37:24 +0200

    Added Touchables Components

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, 
+  Text, 
+  TouchableWithoutFeedback, 
+  TouchableOpacity, 
+  TouchableHighlight, 
+  View, 
+  Image } from 'react-native';
 
 export default function App() {
 
@@ -10,13 +16,25 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <Text numberOfLines={1} onPress={handlePress}>a relatively relative really rally longly long texty text that is long and a lot of text written in the text tag.</Text>
       {/* <Image source = {require('./assets/icon.png')} /> */}
-      <Image 
-      blurRadius={10}
-      fadeDuration = {1000}
-      source = {{
-        width: 200,
-        height: 300,
-        uri: 'https://picsum.photos/200/300'}} />
+
+      {/* <TouchableWithoutFeedback  */}
+      {/* <TouchableOpacity */}
+      <TouchableHighlight
+      // <TouchableNativeFeedback
+        onPress = {() => {console.log("Image tapped")}}
+        onLongPress = {() => {console.log("Image Long Pressed")}}>
+        <Image 
+        blurRadius={10}
+        fadeDuration = {1000}
+        source = {{
+          width: 200,
+          height: 300,
+          uri: 'https://picsum.photos/200/300'}} />
+        {/* </TouchableNativeFeedback> */}
+      </TouchableHighlight>
+      {/* </TouchableOpacity> */}
+      {/* </TouchableWithoutFeedback> */}
+
 
       <StatusBar style="auto" />
     </View>
@@ -26,7 +44,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
     justifyContent: 'center',
