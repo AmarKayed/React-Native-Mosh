991df664a9377133372b8670383882810cc0fdef
Amar Kayed <amar.kayed18@gmail.com>
2022-02-26 20:45:11 +0200

    Started Components In React Native

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text numberOfLines={1} onPress={()=>{console.log("text has been clicked")}}>a relatively relative really rally longly long texty text that is long and a lot of text written in the text tag.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
